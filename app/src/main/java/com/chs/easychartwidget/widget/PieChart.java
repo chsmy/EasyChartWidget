866f864d4f8d2cca2298db2d4d3c4d1b146b0b66
caoweiaaa <13161083183@163.com>
2016-09-21 10:17:45 +0800

    增加饼状图的点击事件

--- app/src/main/java/com/chs/easychartwidget/widget/PieChart.java
+++ app/src/main/java/com/chs/easychartwidget/widget/PieChart.java
@@ -191,13 +191,15 @@ public class PieChart extends View {
                 float x = event.getX()-(mTotalWidth/2);
                 float y = event.getY()-(mTotalHeight/2);
                 float touchAngle = 0;
-                if (x<0&&y<0){
+                if (x<0&&y<0){  //2 象限
                     touchAngle += 180;
-                }else if (y<0&&x>0){
+                }else if (y<0&&x>0){  //1象限
                     touchAngle += 360;
-                }else if (y>0&&x<0){
+                }else if (y>0&&x<0){  //3象限
                     touchAngle += 180;
                 }
+                //Math.atan(y/x) 返回正数值表示相对于 x 轴的逆时针转角，返回负数值则表示顺时针转角。
+                //返回值乘以 180/π，将弧度转换为角度。
                 touchAngle +=Math.toDegrees(Math.atan(y/x));
                 if (touchAngle<0){
                     touchAngle = touchAngle+360;
